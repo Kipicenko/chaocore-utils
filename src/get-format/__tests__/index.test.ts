09135b3113a8247204e2f10b4ec547f38afe222b
Kipicenko <heestons@gmail.com>
2023-11-13 18:21:45 +0400

    corrected test for function getFormat

--- src/get-format/__tests__/index.test.ts
+++ src/get-format/__tests__/index.test.ts
@@ -1,6 +1,12 @@
 import { getFormat } from "get-format";
 
-test("should return .jpg or jpg if dot - false", () => {
-    expect(getFormat("avatar.jpg")).toEqual(".jpg");
-    expect(getFormat("avatar.jpg", false)).toEqual("jpg");
+describe("getFormat", () => {
+    test("should return .jpg or jpg if dot - false", () => {
+        expect(getFormat("avatar.jpg")).toEqual(".jpg");
+        expect(getFormat("avatar.jpg", false)).toEqual("jpg");
+    });
+
+    test("should return empty string if there is no extension", () => {
+        expect(getFormat("avatar")).toEqual("");
+    });
 });
